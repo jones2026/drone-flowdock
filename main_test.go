1d5910eb5939a2a598882638c18d62fa09020c71
John Jones <jones2026@gmail.com>
2019-08-31 11:40:37 -0500

    feat: asserting proper exit code

---
+++ main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestExitWithBadResponseStatus(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}))
+	defer ts.Close()
+
+	var raw []byte
+
+	origLogFatalf := logFatalf
+
+	// After this test, replace the original fatal function
+	defer func() { logFatalf = origLogFatalf }()
+
+	var actualError string
+	logFatalf = func(format string, args ...interface{}) {
+		if len(args) > 0 {
+			actualError = fmt.Sprintf(format, args)
+		} else {
+			actualError = format
+		}
+	}
+
+	postMessage(raw, nil, ts.URL)
+	expectedError := "Failed to post message, flowdock api returned: [503 Service Unavailable]"
+	if actualError == expectedError {
+		return
+	}
+	t.Fatalf("Expected error:\n%s\ngot:\n%s", expectedError, actualError)
+}
